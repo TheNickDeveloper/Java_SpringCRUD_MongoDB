75670e4fc3e1bdc6179ce9253d77a41b3293ca6a
TheNickDeveloper <34956492+TheNickDeveloper@users.noreply.github.com>
2019-08-06 08:17:45 +0800

    Add local mongo data set

--- springBoot/src/main/java/com/nick/Dao/MongoStudentDaoImp.java
+++ springBoot/src/main/java/com/nick/Dao/MongoStudentDaoImp.java
@@ -1,18 +1,48 @@
 package com.nick.Dao;
 
+import com.mongodb.*;
 import com.nick.Entity.Student;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 @Repository
 @Qualifier("MongoData")
 public class MongoStudentDaoImp implements IStudentDao {
 
-    Connection mongoConn;
+    private static Map sudents;
+
+    public MongoStudentDaoImp() throws UnknownHostException {
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        DB db = mongo.getDB("School");
+
+        DBCollection coll = db.getCollection("Student");
+        DBCursor cur = coll.find();
+        int count = 1;
+        while (cur.hasNext()){
+             BasicDBObject obj = (BasicDBObject) cur.next();
+
+             //todo, cannot move to next for some reason, may need to check
+             sudents.put(count, new Student(obj.getInt("id")
+                     ,obj.getString("name")
+                     ,obj.getString("course")));
+             count++;
+        }
+    }
+
+    private DBObject createDBObject(Student student) {
+        BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
+
+        docBuilder.append("_id", student.getId());
+        docBuilder.append("name", student.getName());
+        docBuilder.append("course", student.getCourse());
+        return docBuilder.get();
+    }
+
 
     @Override
     public Collection<Student> getAllStudents() {
