74e13b30de4a373607e83c431953e258c4b8946e
TheNickDeveloper <34956492+TheNickDeveloper@users.noreply.github.com>
2019-08-06 18:28:43 +0800

    Simple MongoDB CURD with springboot

--- springBoot/src/main/java/com/nick/Dao/MongoStudentDaoImp.java
+++ springBoot/src/main/java/com/nick/Dao/MongoStudentDaoImp.java
@@ -6,15 +6,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 @Repository
 @Qualifier("MongoData")
 public class MongoStudentDaoImp implements IStudentDao {
 
-    private static Map sudents;
+    private static Map<Integer, Student> students = new HashMap<Integer, Student>();
 
     public MongoStudentDaoImp() throws UnknownHostException {
         MongoClient mongo = new MongoClient("localhost", 27017);
@@ -22,54 +22,39 @@ public class MongoStudentDaoImp implements IStudentDao {
 
         DBCollection coll = db.getCollection("Student");
         DBCursor cur = coll.find();
-        int count = 1;
         while (cur.hasNext()){
              BasicDBObject obj = (BasicDBObject) cur.next();
-
-             //todo, cannot move to next for some reason, may need to check
-             sudents.put(count, new Student(obj.getInt("id")
-                     ,obj.getString("name")
-                     ,obj.getString("course")));
-             count++;
+             students.put(obj.getInt("id"), new Student(obj.getInt("id"),
+                     obj.getString("name"),
+                     obj.getString("course")));
         }
     }
 
-    private DBObject createDBObject(Student student) {
-        BasicDBObjectBuilder docBuilder = BasicDBObjectBuilder.start();
-
-        docBuilder.append("_id", student.getId());
-        docBuilder.append("name", student.getName());
-        docBuilder.append("course", student.getCourse());
-        return docBuilder.get();
-    }
-
-
     @Override
-    public Collection<Student> getAllStudents() {
-        return new ArrayList<Student>(){
-            {
-                add(new Student(1,"Mario","nothing"));
-            }
-        };
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
     }
 
     @Override
-    public Student getStudentById(int id) {
-        return null;
+    public Student getStudentById(int id){
+        return this.students.get(id);
     }
 
     @Override
     public void removeStudentById(int id) {
-
+        this.students.remove(id);
     }
 
     @Override
-    public void updateStudent(Student student) {
-
+    public void updateStudent(Student student){
+        Student s = students.get(student.getId());
+        s.setCourse(student.getCourse());
+        s.setName(student.getName());
+        this.students.put(s.getId(),student);
     }
 
     @Override
     public void insertStudent(Student student) {
-
+        this.students.put(student.getId(),student);
     }
 }
